266efa2cd25eabe9f90fd213e3c327754b44143f
Przemek-12 <pecework@gmail.com>
2020-04-01 19:30:03 +0200

    commit

--- src/main/java/org/openjfx/mavenfx/session/HibernateSession.java
+++ src/main/java/org/openjfx/mavenfx/session/HibernateSession.java
@@ -27,7 +27,12 @@ public class HibernateSession {
 				        .setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect")
 				        .setProperty("hibernate.show_sql", "true")
 				        .setProperty("hibernate.connection.pool_size", "10");
+				
 				        
+						//The central service API, aside from the services themselves, is the org.hibernate.service.ServiceRegistry interface. 
+						//The main purpose of a service registry is to hold, manage and provide access to services.
+						//Service registries are hierarchical. Services in one registry can depend on and utilize services in that same registry as well as any parent registries.
+						//Services are properties from configuration
 				        StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(config.getProperties());
 				        sessionFactory = config.buildSessionFactory(builder.build());
 			
